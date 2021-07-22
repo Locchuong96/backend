d0bbe216f4cee47e9af84c8e903a2ec3d02c5805
Loc Chuong <73679364+Locchuong96@users.noreply.github.com>
2021-07-22 18:40:39 +0700

    Add files via upload

---
+++ Flask/flask_restapi17/client.py
@@ -0,0 +1,20 @@
+import requests 
+
+url = "http://127.0.0.1:5000/"
+
+data = [
+          {"likes":70240,"name":"Tom and Jerry","views":2000},
+          {"likes":5054,"name":"HellBoy","views":5450},
+         {"likes":3423,"name":"Robinson Curse","views":76400},
+         {"likes":343423,"name":"Robin Hood","views":434400},
+]
+
+for i in range(len(data)):
+    print("http://127.0.0.1:5000/video/"+ str(i))
+    response = requests.put("http://127.0.0.1:5000/video/"+ str(i), data[i])
+    print(response.json())
+
+input("Enter to continue") # Hold until you press enter
+response = requests.get("http://127.0.0.1:5000/video/2")
+print(response.text)
+
